0ba6e2ad03d841c3f1e2b72d92eea04b36acf4f5
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-04 11:04:25 +0530

    update the store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/MoviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const response = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?&page=1",
       API_OPTIONS
     );
     const data = await response.json();
-    console.log(data.results);
+    dispatch(addNowPlayingMovies(data.results));
   };
 
   useEffect(() => {
