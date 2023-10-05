d98e1a5a71d2bd91d96c9d9d9edadb3f564a888c
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-05 12:15:44 +0530

    fetching trailer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,14 +9,14 @@ const MainContainer = () => {
   if (!movies) return;
   const mainMovie = movies[0];
 
-  const { original_title, overview } = mainMovie;
+  const { original_title, overview, id } = mainMovie;
 
   console.log(mainMovie);
 
   return (
     <div>
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/MoviesSlice";
 
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+  const trailerKey = useSelector((store) => store.movies?.trailerVideo);
+  const getMovieVideos = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    );
+
+    const data = await response.json();
+
+    const trailers = data.results.filter(
+      (trailer) => trailer.type === "Trailer"
+    );
+
+    const finalTrailer = trailers.length ? trailers[0] : data.results[0];
+
+    dispatch(addTrailerVideo(finalTrailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        src={`https://www.youtube.com/embed/${trailerKey?.key}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
