1614010bdfb53bae9204cee068e662f4e49072a7
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-05 11:27:04 +0530

    videoTitle

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,12 +9,14 @@ const MainContainer = () => {
   if (!movies) return;
   const mainMovie = movies[0];
 
+  const { original_title, overview } = mainMovie;
+
   console.log(mainMovie);
 
   return (
     <div>
+      <VideoTitle title={original_title} overview={overview} />
       <VideoBackground />
-      <VideoTitle />
     </div>
   );
 };
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const VideoTitle = () => {
-  return <div>VideoTitle</div>;
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="pt-36 px-12">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div className="">
+        <button className="bg-gray-500 text-black p-4 px-12 text-xl bg-opacity-50 rounded-md mr-2">
+          ▶️ Play
+        </button>
+        <button className="bg-gray-500 text-black p-4 px-12 text-xl bg-opacity-50 rounded-md">
+          More info
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default VideoTitle;
