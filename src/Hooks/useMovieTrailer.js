2a4b6ddff1a5e26bd7841e79f0963318965e5d37
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-11 10:05:01 +0530

    custome hook - useMovieTrailer

---
+++ src/Hooks/useMovieTrailer.js
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/MoviesSlice";
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  const getMovieVideos = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    );
+
+    const data = await response.json();
+
+    const trailers = data.results.filter(
+      (trailer) => trailer.type === "Trailer"
+    );
+
+    const finalTrailer = trailers?.length ? trailers[0] : data.results[0];
+
+    dispatch(addTrailerVideo(finalTrailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+};
+
+export default useMovieTrailer;
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,32 +1,10 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/MoviesSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../Hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
   const trailerKey = useSelector((store) => store.movies?.trailerVideo);
-  const getMovieVideos = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-
-    const data = await response.json();
-
-    const trailers = data.results.filter(
-      (trailer) => trailer.type === "Trailer"
-    );
-
-    const finalTrailer = trailers.length ? trailers[0] : data.results[0];
-
-    dispatch(addTrailerVideo(finalTrailer));
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
-
+  useMovieTrailer(movieId);
   return (
     <div>
       <iframe
