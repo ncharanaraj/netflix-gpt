1d5222dbe8bdb50d38de83ef0aa4305a89bfe463
Charanaraj N <goudacharanraj760@gmail.com>
2023-10-11 11:42:49 +0530

    movie card

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,8 @@ export const AVATAR = "https://avatars.githubusercontent.com/u/43924299?v=4";
 export const BACKGROUND =
   "https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_small.jpg";
 
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
 export const API_OPTIONS = {
   method: "GET",
   headers: {
